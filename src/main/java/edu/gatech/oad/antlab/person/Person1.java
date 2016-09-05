1c1155fcb31973ddb8640702db4f467dd21b2cd7
Jitae Kim <kimjt1226@gmail.com>
2016-09-04 20:59:01 -0400

    Person1.java editied by Jitae Kim

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -31,7 +31,9 @@ public class Person1 {
 	 */
 	private String calc(String input) {
 	  //Person 1 put your implementation here
-	  return null;
+	  String rv;
+	  rv = input.substring(2,input.length()) + input.substring(0,2);
+	  return rv;	
 	}
 	
 	/**
